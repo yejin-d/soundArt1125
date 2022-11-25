c252e56861db2be7fa86422869138a7ddf5e723c
ejr6508 <117976672+ejr6508@users.noreply.github.com>
2022-11-25 17:12:14 +0900

    ok

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var wave;
+var playing=false;
+var button;
+function setup() {
+  createCanvas(720, 256);
+  wave=new p5.Oscillator();
+wave.setType('sine');
+//wave.start();
+wave.freq(440);
+wave.amp(0.1);
+
+button=createButton('play/pause');
+button.mousePressed(toggle);
+}
+function draw() {
+}
+function toggle(){
+  
+  if(!playing){
+    wave.start();
+    wave.amp(0.5,1);
+    playing=true;
+  }else{
+    wave.amp(0,1);
+    playing=false;
+  }
+  }
