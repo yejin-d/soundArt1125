99802f0c898b2c12e02f220d46b8cbd84273aee9
ejr6508 <117976672+ejr6508@users.noreply.github.com>
2022-12-02 16:00:06 +0900

    changed3

--- sketch copy.js
+++ sketch copy.js
@@ -1,18 +1,19 @@
 let r, g, b;
 function setup(){
-r= random(50, 255);
-g= random(0, 200);
-b=random(50, 255);
+//r= random(50, 255);
+//g= random(0, 200);
+//b=random(50, 255);
 }
 
 function draw(){
-background(255);
+//background(255);
 //console.log('draw');
-text(accelerationX+", "accelerationY", "+accelerationZ);
+//text(accelerationX+", "accelerationY", "+accelerationZ);
 }
 
 function deviceMoved(){
-r= map(accelerationX, -90, 90, 100, 175);
-g= map(accelerationY, -90, 90, 100, 200);
-b= map(accelerationZ, -90, 90, 100, 200);
+  background(accelerationX);
+//r= map(accelerationX, -90, 90, 100, 175);
+//g= map(accelerationY, -90, 90, 100, 200);
+//b= map(accelerationZ, -90, 90, 100, 200);
 }
